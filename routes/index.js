7c0c8ecd454d778fd779954bcf1d90ad74e6bddc
Adi7893 <amakwan9@my.centennialcollege.ca>
2023-02-09 21:51:41 -0500

    update

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home'})
 });
 
+router.get('/about', function(req, res, next) {
+  res.render('about_me', { title: 'About Me'})
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects page' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services page' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me' });
+});
+
 module.exports = router;
